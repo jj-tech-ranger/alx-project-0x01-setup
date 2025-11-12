e061a4b8497c2183f45b84f4f1779f00f372a8f4
Jemimah Jemutai <jemutaijemimah@gmail.com>
2025-11-12 15:02:43 +0300

    Create Footer.tsx

---
+++ alx-project-0x01/components/layout/Footer.tsx
@@ -0,0 +1,11 @@
+const Footer: React.FC = () => {
+  return (
+    <footer className="bg-gray-800 text-white py-6 mt-auto">
+      <div className="container mx-auto text-center">
+        <p className="text-sm">&copy; 2025 MyApp. All rights reserved.</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
