91df554db409da5cf29901dc0331ab9a92470e50
Jemimah Jemutai <jemutaijemimah@gmail.com>
2025-11-12 16:09:08 +0300

    Update index.ts

--- alx-project-0x01/interfaces/index.ts
+++ alx-project-0x01/interfaces/index.ts
@@ -15,9 +15,18 @@ export interface UserProps {
     suite: string;
     city: string;
     zipcode: string;
+    geo: {
+      lat: string;
+      lng: string;
+    };
   };
   phone: string;
   website: string;
+  company: {
+    name: string;
+    catchPhrase: string;
+    bs: string;
+  };
 }
 
 export interface PostData {
